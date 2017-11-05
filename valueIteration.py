76355fffcbd02d936eea1f2a365eb9ff8ef1a529
Hasan Khan <hk4cd@virginia.edu>
2017-11-05 00:10:49 -0400

    ValIter w/ wind w/ comments

--- valueIteration.py
+++ valueIteration.py
@@ -1,11 +1,6 @@
 ## Hasan Khan, hk4cd | Zach Danz, zyds4
 ## Value Iteration
 
-def noWind():
-
-	currPos = (0, 0)
-	findUtility(currPos)
-
 def findUtility(currPos):
 	x, y = currPos
 	print " \n CURRENT POSITION: (",x,",",y,"), CURR VALUE: ", board[x][y] 
@@ -15,26 +10,21 @@ def findUtility(currPos):
 		return board[x][y]
 
 	marked[x][y] = 1
-	#print "marked:", marked
-	
 	currentReward = -1
 
 	if currPos == (3,6):
 		#print "HIT FINAL STATE!*******************************"
 		currentReward = 0
-		utility = currentReward
-		board[x][y] = utility
+		board[x][y] = 0
 		return board[x][y]
 
-	#print "currentReward:", currentReward
 	neighbors = findNeighbors(board, currPos)
 	print "Neighbors of this pos: ", neighbors
 	allRewards = []
 
-	#calculate action values, based on the given neighbors
-	# [N, S, E, W, NE, NW, SE, SW]
-
-	#print "		Finding utilities of nieghbors..."
+	
+	##Finding utilities of nieghbors
+	## [N, S, E, W, NE, NW, SE, SW]
 		
 	if (neighbors[0] != False):
 		allRewards.append(1*findUtility(neighbors[0]))
@@ -70,11 +60,12 @@ def findUtility(currPos):
 
 	utility = currentReward+(1*bestReward)
 
-	print "	UTILITY of ", currPos, " is :", utility
+	print "	Utility of ", currPos, " is :", utility
 
 	board[x][y] = utility
 
 	return board[x][y]	
+
 
 def findNeighbors(board, currPos):
 	''' Finds and returns all eligible neighbors of the current position'''
@@ -88,82 +79,78 @@ def findNeighbors(board, currPos):
 			print "		~wind~, new curr position: ", x, y
 
 	n= [] ## a list of coordinate tuples of eligible neighbors
-	## Structure: [N, S, E, W | SE, SW, NE, NW]
+	
+	## Structure: [N, S, E, W, SE, SW, NE, NW]
 
 	if y > 0 and x>=0 and y-1>=0:  
-		n.append((x, y-1)) # North
+		n.append((x, y-1)) ## North
 		#print "		appending north"
 	else: n.append(False)
 
 	if y+1 < bLength and x>=0 and y+1>=0:  
-		n.append((x, y+1)) # South
+		n.append((x, y+1)) ## South
 		#print "		appending south"
 	else: n.append(False)
 
 	if x+1 < bWidth and x+1>=0 and y>=0:  
-		n.append((x+1, y)) # East
+		n.append((x+1, y)) ## East
 		#print "		appending east" 
 	else: n.append(False)
 
 	if x > 0 and x-1>=0 and y>=0:  
-		n.append((x-1, y)) # West
+		n.append((x-1, y)) ## West
 		#print "		appending west"
 	else: n.append(False)
 
-
 	if y+1 < bLength and x+1 < bWidth and x+1>=0 and y+1>=0: 
-		n.append((x+1, y+1)) # Southeast
+		n.append((x+1, y+1)) ## Southeast
 		#print "		appending southeast"
 	else: n.append(False)
 
 	if y+1 < bLength and x > 0 and x-1>=0 and y+1>=0: 
-		n.append((x-1, y+1)) # Southwest
+		n.append((x-1, y+1)) ## Southwest
 		#print "		appending southwest"
 	else: n.append(False)
 
 	if y > 0 and x+1 < bWidth and x+1>=0 and y-1>=0 : 
-			n.append((x+1, y-1))
+			n.append((x+1, y-1)) ## Northeast
 			#print "		appending northeast"
 	else: n.append(False)
 
 	if y > 0 and x > 0 and x-1>=0 and y-1>=0: 
-			n.append((x-1, y-1))
+			n.append((x-1, y-1)) ## Northwest
 			#print "		appending northwest"
 	else: n.append(False)
-
-	#n.append((x, y))
+	#n.append((x, y)) Dont need to do this, accouned for in findUtility
 	return n  
 
 ##########################################################
 ##########################################################
 
-from pandas import *
+from pandas import * # Makes 2D arrays render nice in terminal using DataFrame
 
 global marked
 global windFactor
 
+## Board dimensions
 bLength = 7
 bWidth = 7
 
-windFactor = 1
+windFactor = 1 # set to 0, 1, or 2
 
-
+## Init board
 board = [[0] * bLength for i in range(bWidth)]
-board[2][5] = 5
-#board[row][col]
+## board[row][col] indexing
 print DataFrame(board)
-
-#'''
-
+## Run value iteration on loop 
 for i in range (0, 1):
 	
 	marked = [[0] * bLength for i in range(bWidth)]
-	#board[6][3] = 0
-	#marked[3][6] = 1
-	noWind()
+	currPos = (0, 0)
+	findUtility(currPos)
 	print ""
 	print DataFrame(board)
-#'''
+
 
 
 
