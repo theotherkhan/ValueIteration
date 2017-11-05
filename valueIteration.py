c191c0754a0354540d53e7f9b72a673b262cf482
Zachary Danz <zsd4yr@virginia.edu>
2017-11-05 11:19:37 -0500

    I cannot believe this is working

--- valueIteration.py
+++ valueIteration.py
@@ -1,158 +1,186 @@
 ## Hasan Khan, hk4cd | Zach Danz, zyds4
 ## Value Iteration
+from pandas import * # Makes 2D arrays render nice in terminal using DataFrame
+
+global FINAL_STATE
+FINAL_STATE = 3,6
+global northerlyWind
+global southerlyWind
+global easterlyWind
+global westerlyWind
+northerlyWind = 0,0,0,0,0,0,0 
+southerlyWind = 0,0,0,0,0,0,0 
+easterlyWind = 0,0,0,0,0,0,0 
+westerlyWind = 0,0,0,0,0,0,0 
+global marked
+
+######################## UTILITY ###########################
+####################### FUNCITONS ##########################
 
 def findUtility(currPos):
-	x, y = currPos
-	print " \n CURRENT POSITION: (",x,",",y,"), CURR VALUE: ", board[x][y] 
+	#print "\n"
+	#print DataFrame(board)
+	#print DataFrame(marked)
+
+
+	x = currPos[0]
+	y = currPos[1]
+	#print " \n CURRENT POSITION: (",x,",",y,"), CURR VALUE: ", board[x][y] 
 	
-	if marked[x][y] == 1:
-		print "		Already marked this position"
+	#if marked[x][y] == 1:
+		#print "\tAlready marked this position"
+		#return board[x][y]
+
+	if board[x][y] > 0:
+		#cd GOo	marked[x][y] = 0
 		return board[x][y]
 
 	marked[x][y] = 1
 	currentReward = -1
 
-	if currPos == (3,6):
+	if currPos == FINAL_STATE:
 		#print "HIT FINAL STATE!*******************************"
-		currentReward = 0
+		currentReward = 100
 		board[x][y] = 0
 		return board[x][y]
 
-	neighbors = findNeighbors(board, currPos)
-	print "Neighbors of this pos: ", neighbors
+	neighbors, ways = findSucessors(board, currPos)
+	#print "Neighbors of this pos: ", neighbors
 	allRewards = []
-
 	
 	##Finding utilities of nieghbors
 	## [N, S, E, W, NE, NW, SE, SW]
-		
-	if (neighbors[0] != False):
-		allRewards.append(1*findUtility(neighbors[0]))
-	
-	if (neighbors[1] != False):
-		allRewards.append(1*findUtility(neighbors[1]))
-	
-	if (neighbors[2] != False):	
-		allRewards.append(1*findUtility(neighbors[2]))
-	
-	if (neighbors[3] != False):
-		allRewards.append(1*findUtility(neighbors[3]))	
-	
-	if (neighbors[4] != False):
-		allRewards.append(1*findUtility(neighbors[4]))
-	
-	if (neighbors[5] != False):
-		allRewards.append(1*findUtility(neighbors[5]))
-	
-	if (neighbors[6] != False):
-		allRewards.append(1*findUtility(neighbors[6]))
 
-	if (neighbors[7] != False):
-		allRewards.append(1*findUtility(neighbors[7]))
-	
-	allRewards.append(board[x][y])
+	for  onceAndFutureKing in neighbors:
+		if onceAndFutureKing != False :
+			if marked[onceAndFutureKing[0]][onceAndFutureKing[1]] == 0: #not being calculated
+				allRewards.append(1*findUtility(onceAndFutureKing))
+		
+	allRewards.append(board[x][y]) #stay
 		
 	bestReward = 0
 	
-	if len(allRewards)!=0:
+	if len(allRewards)>0:
 		#print "allRewards of ", currPos, ":", allRewards
 		bestReward = max(allRewards)	
 
 	utility = currentReward+(1*bestReward)
 
-	print "	Utility of ", currPos, " is :", utility
+	#print "\tUtility of", currPos, "is :", utility
 
 	board[x][y] = utility
+	marked[x][y] = 0
 
 	return board[x][y]	
 
 
-def findNeighbors(board, currPos):
+def findSucessors(board, currPos):
 	''' Finds and returns all eligible neighbors of the current position'''
 
 	x=currPos[0]	
 	y=currPos[1]
 
-	if windFactor !=0 and x in range (3,6): 	
-		if y-windFactor >= -2:
-			y = y-windFactor
-			print "		~wind~, new curr position: ", x, y
-
-	n= [] ## a list of coordinate tuples of eligible neighbors
+	n = [] ## a list of coordinate tuples of eligible neighbors
 	
 	## Structure: [N, S, E, W, SE, SW, NE, NW]
+	dirs = []
 
 	if y > 0 and x>=0 and y-1>=0:  
-		n.append((x, y-1)) ## North
-		#print "		appending north"
-	else: n.append(False)
+		n.append((x, y-1)) 
+		dirs.append('W')	## North
+	else: 
+		n.append(False)
+		dirs.append(u'\8635')
 
 	if y+1 < bLength and x>=0 and y+1>=0:  
-		n.append((x, y+1)) ## South
-		#print "		appending south"
-	else: n.append(False)
+		n.append((x, y+1))
+		dirs.append('EAST')	## South
+	else: 
+		n.append(False)
+		dirs.append(u'\8635')
 
 	if x+1 < bWidth and x+1>=0 and y>=0:  
-		n.append((x+1, y)) ## East
-		#print "		appending east" 
-	else: n.append(False)
+		n.append((x+1, y))
+		dirs.append('S')	## East
+	else: 
+		n.append(False)
+		dirs.append(u'\8635')
 
 	if x > 0 and x-1>=0 and y>=0:  
-		n.append((x-1, y)) ## West
-		#print "		appending west"
-	else: n.append(False)
+		n.append((x-1, y)) 
+		dirs.append('N')	## West
+	else: 
+		n.append(False)
+		dirs.append(u'\8635')
 
 	if y+1 < bLength and x+1 < bWidth and x+1>=0 and y+1>=0: 
-		n.append((x+1, y+1)) ## Southeast
-		#print "		appending southeast"
-	else: n.append(False)
+		n.append((x+1, y+1))
+		dirs.append('SE')	## Southeast
+	else: 
+		n.append(False)
+		dirs.append(u'\8635')
 
 	if y+1 < bLength and x > 0 and x-1>=0 and y+1>=0: 
-		n.append((x-1, y+1)) ## Southwest
-		#print "		appending southwest"
-	else: n.append(False)
+		n.append((x-1, y+1))
+		dirs.append('NE')	## Southwest
+	else: 
+		n.append(False)
+		dirs.append(u'\8635')
 
 	if y > 0 and x+1 < bWidth and x+1>=0 and y-1>=0 : 
-			n.append((x+1, y-1)) ## Northeast
-			#print "		appending northeast"
-	else: n.append(False)
+		n.append((x+1, y-1))
+		dirs.append('SW')	## Northeast
+	else: 
+		n.append(False)
+		dirs.append(u'\8635')
 
 	if y > 0 and x > 0 and x-1>=0 and y-1>=0: 
-			n.append((x-1, y-1)) ## Northwest
-			#print "		appending northwest"
-	else: n.append(False)
-	#n.append((x, y)) Dont need to do this, accouned for in findUtility
-	return n  
+		n.append((x-1, y-1))
+		dirs.append('NW')	## Northwest
+	else: 
+		n.append(False)
+		dirs.append(u'\8635')
+	
+	return n, dirs  
 
-##########################################################
-##########################################################
-
-from pandas import * # Makes 2D arrays render nice in terminal using DataFrame
-
-global marked
-global windFactor
+########################  MAIN #############################
+####################### FUNCITON ###########################
 
 ## Board dimensions
 bLength = 7
 bWidth = 7
 
-windFactor = 1 # set to 0, 1, or 2
-
-## Init board
-board = [[0] * bLength for i in range(bWidth)]
+## Init boards
+board = [[0] * bLength for i in range(bWidth)] #to store the results
+marked = [[0] * bLength for i in range(bWidth)] #to store current visitation
+policy = [[0] * bLength for i in range(bWidth)]
+board[FINAL_STATE[0]][FINAL_STATE[1]] = 100
 ## board[row][col] indexing
 print DataFrame(board)
+
 ## Run value iteration on loop 
 for i in range (0, 1):
-	
-	marked = [[0] * bLength for i in range(bWidth)]
-	currPos = (0, 0)
+	currPos = (0,0)
 	findUtility(currPos)
-	print ""
+	print "\n"
 	print DataFrame(board)
 
+for i in range(0, bWidth):
+	for j in range(0, bLength):
+		currPos = i,j
+		myValue = board[i][j]
+		myDir = "."
+		waystones, ways = findSucessors(board, currPos)
+		for p in range(0, len(waystones)):
+			path = waystones[p]
+			if path != False:
+				way = ways[p]
+				contender = board[path[0]][path[1]]
+				#print "for", i, j, "we have a value of", myValue, "and direction", myDir, "with contender", contender, "and direction", way
+				if contender > myValue:
+					myValue = board[path[0]][path[1]]
+					myDir = way
+		policy[i][j] = myDir
 
-
-
-
-
+print "\n"
+print DataFrame(policy)
